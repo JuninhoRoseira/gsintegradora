cdecc2ce65c571d0fd9c2c9b599502d087d0f797
JuninhoRoseira <juninhoroseira@gmail.com>
2015-01-14 12:12:18 -0200

    Acerto layout

--- GSIntegradora.Dominio/Entidades/Usuario.cs
+++ GSIntegradora.Dominio/Entidades/Usuario.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GSIntegradora.Dominio.Entidades
 {
@@ -12,7 +8,7 @@ namespace GSIntegradora.Dominio.Entidades
 		public virtual String Nome { get; set; }
 		public virtual string Login { get; set; }
 		public virtual string Email { get; set; }
-		public virtual bool Ativo { get; set; }
 		public virtual string Senha { get; set; }
+		public virtual bool Ativo { get; set; }
 	}
 }
--- GSIntegradora.Infraestrutura/NHibernate/Mapeamentos/UsuarioMapeamento.cs
+++ GSIntegradora.Infraestrutura/NHibernate/Mapeamentos/UsuarioMapeamento.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentNHibernate.Mapping;
+﻿using FluentNHibernate.Mapping;
 using GSIntegradora.Dominio.Entidades;
 
 namespace GSIntegradora.Infraestrutura.NHibernate.Mapeamentos
@@ -20,7 +15,7 @@ namespace GSIntegradora.Infraestrutura.NHibernate.Mapeamentos
 			this.Map(u => u.Login).Not.Nullable();
 			this.Map(u => u.Email).Not.Nullable();
 			this.Map(u => u.Senha).Not.Nullable();
-			this.Map(u => u.Ativo).Default("1");
+			this.Map(u => u.Ativo);
 
 		}
 
